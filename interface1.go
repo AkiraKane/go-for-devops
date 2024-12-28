8ff5a5bb50eb2a42d550b30debf59de87227d730
Akira Kaneshiro <vampire1987116hm@gmail.com>
2024-12-28 10:46:48 -0800

    Create interface1.go

---
+++ interface1.go
@@ -0,0 +1,33 @@
+package main
+ 
+import (
+    "fmt"
+    "reflect"
+)
+ 
+type Flyer interface{
+    fly() string
+}
+ 
+type Walker interface{
+    walk() string
+}
+ 
+type Bird struct{
+    Name string
+}
+ 
+func (b *Bird)fly() string{
+    return "Flying..."
+}
+ 
+func (b *Bird)walk() string{
+    return "Walking..."
+}
+ 
+func main() {
+    var b = Bird{"Chirper"}
+     
+    fmt.Println(b.fly())     // Flying...
+    fmt.Println(b.walk())    // Walking...  
+}
